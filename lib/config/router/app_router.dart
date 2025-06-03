fe355c29177b870f9b4f764e1150fbc056870c49
Manuela Viola <manuela.viola@gmail.com>
2025-06-03 17:26:10 -0300

    feat: go to notification details

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,5 +2,14 @@ import 'package:go_router/go_router.dart';
 import 'package:push_app/presentation/screens/screens.dart';
 
 final appRouter = GoRouter(
-  routes: [GoRoute(path: '/', builder: (_, _) => const HomeScreen())],
+  routes: [
+    GoRoute(path: '/', builder: (_, _) => const HomeScreen()),
+    GoRoute(
+      path: '/push-details/:messageId',
+      builder:
+          (context, state) => DetailsScreen(
+            pushMessageId: state.pathParameters['messageId'] ?? 'no-id',
+          ),
+    ),
+  ],
 );
